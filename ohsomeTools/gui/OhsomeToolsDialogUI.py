c641f8bf0ef2f5c3f28842d9d1d3272ae39ec506
Julian Psotta <julianpsotta@gmail.com>
2021-07-26 18:35:22 +0200

    rename OhsomeQgisDialogUI to OhsomeToolsDialogUI

--- ohsomeTools/gui/OhsomeToolsDialogUI.py
+++ ohsomeTools/gui/OhsomeToolsDialogUI.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'OhsomeQgisDialogUI.ui'
+# Form implementation generated from reading ui file 'OhsomeToolsDialogUI.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
