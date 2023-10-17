b213d8b6a72c7652463313160912d136c5aee0bd
Merydian <Tillfrankenbach@web.de>
2023-10-17 11:13:35 +0200

    Run pre-commit

--- ohsomeTools/common/request_core.py
+++ ohsomeTools/common/request_core.py
@@ -288,7 +288,9 @@ class ExtractionTaskFunction(QgsTask):
     def postprocess_results(self) -> bool:
         file = self.dlg.lineEdit_output.text()
         if not file:
-            file = QgsProcessingUtils.generateTempFilename(f'Ohsome_{datetime.now()}.csv')
+            file = QgsProcessingUtils.generateTempFilename(
+                f"Ohsome_{datetime.now()}.csv"
+            )
         if not self.result or not len(self.result):
             return False
         if "extractRegion" in self.result:
